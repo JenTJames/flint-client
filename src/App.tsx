07903a7618fab2a868154502becdc1993441da9f
Jen Thomas James <jenthomasjames@gmail.com>
2025-03-09 23:25:36 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="flex min-h-screen bg-secondary justify-center items-center">
+      <Button>Hello Shadcn</Button>
+    </div>
+  );
+};
+
+export default App;
